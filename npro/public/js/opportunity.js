9b402487217123ec758b6f05c57c5c3740932387
ashish-greycube <mr.ashish.shah@gmail.com>
2021-01-20 11:17:52 +0530

    sales_stage filtering

--- npro/public/js/opportunity.js
+++ npro/public/js/opportunity.js
@@ -1,4 +1,14 @@
 frappe.ui.form.on("Opportunity", {
+    setup:function(frm)
+    {
+        frm.set_query('sales_stage', () => {
+        return {
+            filters: {
+                opportunity_type_cf: ['in', [frm.doc.opportunity_type,""]]
+            }
+        }
+    })
+    },
 	refresh: function(frm) {
         frm.set_value('opportunity_from','Customer',true)
 
