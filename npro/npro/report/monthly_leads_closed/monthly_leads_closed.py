5c8cc3ad63fd3dc30447cda01f58502203d4de72
vr-greycube <vijay@greycube.in>
2021-01-24 16:14:41 +0530

    fix: Monthly Leads Closed aet index as Owner

--- npro/npro/report/monthly_leads_closed/monthly_leads_closed.py
+++ npro/npro/report/monthly_leads_closed/monthly_leads_closed.py
@@ -3,6 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
+from frappe.utils import getdate
 import pandas
 
 
@@ -16,18 +17,19 @@ def get_data(filters):
     with fn as
     (
         select
-        ld.name, ld.status,
-        DATE_FORMAT(COALESCE(v.creation, ld.creation),'%%b-%%Y') last_changed,
+        ld.name, u.full_name owner,
+        DATE_FORMAT(LAST_DAY(COALESCE(v.creation, ld.creation)),'%%Y-%%m-%%d') last_changed,
         ROW_NUMBER() over (PARTITION by ld.name order by v.creation DESC) rn,
         v.data 
         from tabLead ld
+        left outer join tabUser u on u.name = ld.lead_owner
         left outer join tabVersion v on v.docname = ld.name 
         and v.ref_doctype = 'Lead'
         and v.data REGEXP '.*"changed":.*().*'
         and v.data  REGEXP concat(',\n(   )("',ld.status,'")\n(  ]).*')
         {where_conditions}
     )
-    select fn.status, fn.last_changed, fn.rn
+    select fn.owner, fn.last_changed, fn.rn
     from fn
     where rn = 1
     """.format(
@@ -35,39 +37,46 @@ def get_data(filters):
         ),
         filters,
         as_dict=True,
-        # debug=True,
+        debug=True,
     )
 
     if not data:
         return [], []
 
-    #
+    # add default 0 for all months
+    for d in pandas.date_range(
+        filters.get("from_date"), filters.get("to_date"), freq="m"
+    ):
+        data.append(
+            {"owner": data[0].owner, "last_changed": d.strftime("%Y-%m-%d"), "rn": 0,}
+        )
+    print(data)
+
     df = pandas.DataFrame.from_records(data)
     import numpy as np
 
     df1 = pandas.pivot_table(
         df,
-        index=["status"],
+        index=["owner"],
         values=["rn"],
         columns=["last_changed"],
-        aggfunc="count",
+        aggfunc=sum,
         fill_value=0,
         margins=True,
         dropna=True,
     )
-    df1
-    df1.drop(index="All", axis=0, inplace=True)
-    df1.columns = [frappe.scrub(d) for d in df1.columns.to_series().str[1]]
-    df2 = df1.reset_index()
-    df2
 
-    columns = [dict(label="Status", fieldname="status", fieldtype="Data", width=165)]
+    df1.drop(index="All", axis=0, inplace=True)
+    df1.columns = [d for d in df1.columns.to_series().str[1]]
+    df2 = df1.reset_index()
+
+    columns = [dict(label="Salesman", fieldname="owner", fieldtype="Data", width=165)]
 
     columns += [
         dict(
-            label=frappe.unscrub(
-                col.replace("___", " - ").replace("_+", " +").replace("all", "Total")
-            ),
+            label=frappe.utils.format_date(col, "MMM YYYY")
+            if not col == "All"
+            else "Total",
             fieldname=col,
             fieldtype="Int",
             width=95,
