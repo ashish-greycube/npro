de323c8ec30d590a6224892709577ef8ff98f595
ashish-greycube <mr.ashish.shah@gmail.com>
2021-01-27 09:28:54 +0100

    npro settings

---
+++ npro/npro/doctype/npro_settings/test_npro_settings.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, GreyCube Technologies and Contributors
+# See license.txt
+from __future__ import unicode_literals
+
+# import frappe
+import unittest
+
+class TestNProSettings(unittest.TestCase):
+	pass
