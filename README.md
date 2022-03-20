2fa0030d897a55a9f2e3e19c6fa48d99bacd1bab
vr-greycube <vijay@greycube.in>
2022-03-20 15:33:13 +0530

    feat: add api for current job openings for website
    add child table in NPro Settings for business module to webservice mapping

--- README.md
+++ README.md
@@ -15,6 +15,17 @@ Customization for NPro Pte Ltd
 
 6. Auto Email Report for Reports :  Lead Status Reminder and Opportunity Stage Reminder
 
+
+### Website API for current Open Job Openings
+
+1. Create an api user
+2. Pass Authorization header in call to api method /api/method/npro.integrations.job_openings.job_opening
+```
+    Authorization: api_token:api_secret
+```
+3. Result will be a list of Job Openings
+4. In case of exception, result will have an "exception" property with the exception message. 
+5. Add mapping for Business Module in NPro Settings > Business Module Website Service Mapping
 #### License
 
 MIT
---
+++ npro/integrations/job_openings.py
@@ -0,0 +1,27 @@
+from __future__ import unicode_literals
+import frappe, json
+from werkzeug.wrappers import Response
+from frappe.utils.response import as_json
+
+
+@frappe.whitelist(allow_guest=True)
+def job_opening():
+    """List of current Job Openings in Open status."""
+
+    data = frappe.db.sql(
+        """
+    select 
+        tjo.name, tjo.location_cf , tjo.job_title , tjo.description , tbm.website_service
+    from 
+        `tabJob Opening` tjo
+        left outer join tabOpportunity topp on topp.name  = tjo.opportunity_cf 
+        left outer join `tabBusiness Module Website Service Mapping` tbm on tbm.opportunity_business_module = topp.business_module 
+    where 
+        tjo.status = 'Open'
+    """,
+        as_dict=True,
+    )
+
+    response = as_json()
+    response.data = frappe.as_json(data)
+    return response
