a6975ea422f6eb8411cc9808e597a73429274dcf
ashish-greycube <mr.ashish.shah@gmail.com>
2022-03-30 12:28:30 +0200

    v2 customizations
    add doctypes for consultant post onboarding, npro attachment, consultant post onboarding template, npro attachment type

---
+++ npro/npro/doctype/npro_attachment_type/npro_attachment_type.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Npro Attachment Type', {
+	// refresh: function(frm) {
+
+	// }
+});
