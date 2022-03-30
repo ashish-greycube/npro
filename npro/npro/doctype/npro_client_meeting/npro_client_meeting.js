2f91a2d4841a576c57bd51cad1e7b6e8618d9384
ashish-greycube <mr.ashish.shah@gmail.com>
2022-03-30 16:08:58 +0200

    add doctypes npro client meeting
    npro consultant meeting, npro timesheet

---
+++ npro/npro/doctype/npro_client_meeting/npro_client_meeting.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, GreyCube Technologies and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('NPro Client Meeting', {
+	// refresh: function(frm) {
+
+	// }
+});
