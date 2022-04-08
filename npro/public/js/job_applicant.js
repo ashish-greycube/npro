d6e583dae2a4bc5ec082f911d67bc08b222eaf64
vr-greycube <vijay@greycube.in>
2022-04-08 10:33:29 +0530

    minor: set exchange rate in job offer, job applicant
    use creation date as transaction date

--- npro/public/js/job_applicant.js
+++ npro/public/js/job_applicant.js
@@ -1,7 +1,6 @@
 frappe.ui.form.on('Job Applicant', {
     refresh: function (frm) {
-        if (frm.doc.docstatus == 0)
-            frm.events.set_exchange_rate(frm);
+
     },
 
     current_salary_cf: function (frm) {
@@ -14,14 +13,13 @@ frappe.ui.form.on('Job Applicant', {
             method: "erpnext.setup.utils.get_exchange_rate",
             args: {
                 from_currency: "INR",
-                to_currency: "USD"
+                to_currency: "USD",
+                transaction_date: frm.doc.creation
             },
             callback: function (r) {
                 if (r.message) {
                     frm.set_value('conversion_rate_cf', r.message)
-                    if (frm.doc.current_salary_cf) {
-                        frm.set_value('current_salary_usd_cf', (frm.doc.current_salary_cf || 0) * r.message)
-                    }
+                    frm.set_value('current_salary_usd_cf', (frm.doc.current_salary_cf || 0) * r.message)
                 }
             }
         });
--- npro/public/js/job_offer.js
+++ npro/public/js/job_offer.js
@@ -1,9 +1,5 @@
 frappe.ui.form.on('Job Offer', {
     refresh: function (frm) {
-        if (frm.doc.docstatus == 0)
-            frm.events.set_exchange_rate(frm);
-
-
         frm.events.add_custom_buttons(frm);
     },
 
@@ -37,14 +33,13 @@ frappe.ui.form.on('Job Offer', {
             method: "erpnext.setup.utils.get_exchange_rate",
             args: {
                 from_currency: "INR",
-                to_currency: "USD"
+                to_currency: "USD",
+                transaction_date: frm.doc.creation
             },
             callback: function (r) {
                 if (r.message) {
                     frm.set_value('conversion_rate_cf', r.message)
-                    if (frm.doc.consultancy_fees_offered_cf) {
-                        frm.set_value('consultancy_fees_offered_usd_cf', (frm.doc.consultancy_fees_offered_cf || 0) * r.message)
-                    }
+                    frm.set_value('consultancy_fees_offered_usd_cf', (frm.doc.consultancy_fees_offered_cf || 0) * r.message)
                 }
             }
         });
