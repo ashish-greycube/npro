e4ce74716a79e762631851a878f59aad37b78b4e
vr-greycube <vijay@greycube.in>
2022-05-17 16:55:06 +0530

    feat: add parameters in api call for job openings

--- npro/integrations/job_openings.py
+++ npro/integrations/job_openings.py
@@ -4,10 +4,24 @@ from werkzeug.wrappers import Response
 from frappe.utils.response import as_json
 
 
-
-@frappe.whitelist(allow_guest=True)
-def job_opening():
+@frappe.whitelist(allow_guest=False)
+def job_opening(**args):
     """List of current Job Openings in Open status."""
+    args["search_term"] = "%%{}%%".format(args.get("s"))
+
+    conditions = []
+    if args.get("name"):
+        conditions += [" tjo.name = %(name)s"]
+    if args.get("s"):
+        conditions += [
+            " (tjo.job_title like %(search_term)s or tjo.description like %(search_term)s) "
+        ]
+    if args.get("location"):
+        conditions += [" tjo.location_cf = %(location)s"]
+    if args.get("module"):
+        conditions += [" tbm.website_service = %(module)s"]
+
+    conditions = conditions and " and " + " and ".join(conditions) or ""
 
     data = frappe.db.sql(
         """
@@ -18,35 +32,14 @@ def job_opening():
         left outer join tabOpportunity topp on topp.name  = tjo.opportunity_cf 
         left outer join `tabBusiness Module Website Service Mapping` tbm on tbm.opportunity_business_module = topp.business_module 
     where 
-        tjo.status = 'Open'
-    """,
+        tjo.status = 'Open' {conditions}
+    """.format(
+            conditions=conditions
+        ),
+        args,
         as_dict=True,
     )
 
     response = as_json()
     response.data = frappe.as_json(data)
     return response
-
-
-@frappe.whitelist()
-def job_opening_test():
-    """List of current Job Openings in Open status."""
-    return ""
-    data = frappe.db.sql(
-        """
-    select 
-        tjo.name, tjo.location_cf , tjo.job_title , tjo.description , tbm.website_service
-    from 
-        `tabJob Opening` tjo
-        left outer join tabOpportunity topp on topp.name  = tjo.opportunity_cf 
-        left outer join `tabBusiness Module Website Service Mapping` tbm on tbm.opportunity_business_module = topp.business_module 
-    where 
-        tjo.status = 'Open'
-    """,
-        as_dict=True,
-    )
-
-    response = as_json()
-    response.data = frappe.as_json(data)
-    return response
-
