a4007c504f6d990b24ebfe368ac5d57b6daef6af
vr-greycube <vijay@greycube.in>
2022-06-15 20:14:24 +0530

    fix: percent_complete col in project tasks report

--- npro/npro/report/project_tasks_report/project_tasks_report.py
+++ npro/npro/report/project_tasks_report/project_tasks_report.py
@@ -118,7 +118,7 @@ def get_columns(filters):
         },
         {
             "label": _("Project %"),
-            "fieldname": "percent_progress",
+            "fieldname": "percent_complete",
             "fieldtype": "Percent",
             "width": 110,
         },
